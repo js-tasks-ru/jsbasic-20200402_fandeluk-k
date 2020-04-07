bafdb1693061f165e60cfbc45d476629f0c507ae
Konstantin <fandeluk.k@gmail.com>
2020-04-07 19:15:12 +0300

    FilterSpam

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -3,6 +3,11 @@
  * @param {string} str base
  * @returns {boolean}
  */
-function checkSpam(str) {
-  // ваш код...
-}
+const checkSpam = (str) => {
+  let str2 = str.toLowerCase();
+  if (str2.includes("1xbet") || str2.includes("xxx")) {
+    return true;
+  } else {
+    return false;
+  }
+};
