dab82bfb22146562d0c59cd244c51b047499d01f
Konstantin <fandeluk.k@gmail.com>
2020-04-07 15:42:53 +0300

    +2 задачи

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -14,7 +14,7 @@ function factorial(n) {
   }
   return res;
 }
-factorial(0); // 1
-factorial(1); // 1
-factorial(3); // 6
-factorial(5); // 120
+factorial(0);
+factorial(1);
+factorial(3);
+factorial(5);
