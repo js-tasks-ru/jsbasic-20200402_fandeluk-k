71c5ea2b51549807fa195ccf79e77fd4c6360e5d
Konstantin <fandeluk.k@gmail.com>
2020-04-10 19:54:23 +0300

    Новая задача

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -5,5 +5,11 @@
  * @returns {number[]}
  */
 function filterRange(arr, a, b) {
-  // ваш код...
+  let newArr = [];
+  arr.map((vl, i) => {
+    if (a >= vl || b >= vl) {
+      newArr.push(vl);
+    }
+  });
+  return newArr;
 }
