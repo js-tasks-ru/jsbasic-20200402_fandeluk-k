5f894eaa91a73efd546eb8bb3e4994baf27e0f1c
Konstantin <fandeluk.k@gmail.com>
2020-04-16 12:59:56 +0300

    Последняя задача fix v2

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,24 +3,25 @@
  * @param {Element} table
  */
 function highlight(table) {
-  let tBody = document.querySelector("tbody");
+  let tBody = table.querySelector("tbody");
   let tBodyRows = Array.from(tBody.rows);
-
   tBodyRows.forEach((td) => {
+    console.log(td);
     let dataAvailble = td.cells[3].getAttribute("data-available");
     let userAge = td.cells[1].textContent;
     let genderSwitch = td.cells[2].textContent;
 
     dataAvailble == "true" ? td.classList.add("available") : false;
+    dataAvailble == "false" ? td.classList.add("unavailable") : false;
     dataAvailble == null ? td.setAttribute("hidden", "hidden") : false;
     userAge < 18
       ? td.setAttribute("style", "text-decoration: line-through")
       : false;
 
     if (genderSwitch == "m") {
-      td.classList.add("Male");
+      td.classList.add("male");
     } else {
-      td.classList.add("Flame");
+      td.classList.add("female");
     }
   });
 }
