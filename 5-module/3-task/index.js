ef2f1275fc887ac3e7bdffa2d089e4b41a8aee91
Konstantin <fandeluk.k@gmail.com>
2020-04-20 18:03:28 +0300

    задача

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -2,23 +2,28 @@ function initCarousel() {
   let carouselInner = document.querySelector(".carousel__inner");
   document.querySelector(".carousel__arrow_left").style.display = "none";
 
-  let counter = 1;
+  let counter = 0;
 
   document.addEventListener("click", function (event) {
     let btnRight = event.target.closest(".carousel__arrow_right");
     let btnLeft = event.target.closest(".carousel__arrow_left");
-    counter >= 4
+    counter >= 2
       ? (document.querySelector(".carousel__arrow_right").style.display =
           "none")
       : false;
 
-    if (btnRight && counter < 4) {
-      ++counter;
-      carouselInner.style.transform += `translateX(${-carouselInner.offsetWidth}px`;
+    if (btnRight && counter < 3) {
+      counter++;
+      console.log(counter);
+      carouselInner.style.transform = `translateX(${
+        -carouselInner.offsetWidth * counter
+      }px`;
       document.querySelector(".carousel__arrow_left").style.display = "";
-    } else if (btnLeft && counter > 1) {
-      --counter;
-      carouselInner.style.transform += `translateX(${carouselInner.offsetWidth}px`;
+    } else if (btnLeft && counter > 0) {
+      counter--;
+      carouselInner.style.transform = `translateX(${
+        -carouselInner.offsetWidth * counter
+      }px`;
       document.querySelector(".carousel__arrow_right").style.display = "";
     }
   });
