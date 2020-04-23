a419ab7b50c5ddb50962e3565e10725d4d15c348
Konstantin <fandeluk.k@gmail.com>
2020-04-24 00:00:45 +0300

    Новое решение

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,38 +1,69 @@
 function initCarousel() {
-  let carouselInner = document.querySelector(".carousel__inner");
-  document.querySelector(".carousel__arrow_left").style.display = "none";
-  let slideElement = document.querySelectorAll(".carousel__slide").length;
+  let carouselMain = document.querySelector(".carousel");
+  let carouselInner = carouselMain.querySelector(".carousel__inner");
+  let slideElement = document.querySelectorAll(".carousel__slide").length - 1;
+
+  let arrowLeft = document.querySelector(".carousel__arrow_left");
+  let arrowRight = document.querySelector(".carousel__arrow_right");
 
   let counter = 0;
+  counter == 0 ? (arrowLeft.style.display = "none") : false;
+  carouselMain.addEventListener("click", function (event) {
+    
+    counter > 0
+      ? (arrowLeft.style.display = "")
+      : (arrowLeft.style.display = "none");
+    counter < slideElement
+      ? (arrowRight.style.display = "")
+      : (arrowRight.style.display = "none");
 
-  document.addEventListener("click", function (event) {
-    let btnRight = event.target.closest(".carousel__arrow_right");
-    let btnLeft = event.target.closest(".carousel__arrow_left");
-
-    function showMeArrow() {
-      counter > 0
-        ? (btnLeft.style.display = "")
-        : (btnLeft.style.display = "none");
-      counter < slideElement - 1
-        ? (btnRight.style.display = "")
-        : (btnRight.style.display = "none");
+    function slideWidth() {
+      return (carouselInner.style.transform = `translateX(${
+        -carouselInner.offsetWidth * counter
+      }px`);
     }
 
     function switchSlider() {
-      if (btnRight && counter < slideElement - 1) {
+      if (
+        event.target.closest(".carousel__arrow_right") &&
+        counter <= slideElement
+      ) {
         counter++;
-        carouselInner.style.transform = `translateX(${
-          -carouselInner.offsetWidth * counter
-        }px`;
-      }
-      if (btnLeft && counter > 0) {
+        slideWidth();
+        arrowRight.style.display = "none";
+      } else if (event.target.closest(".carousel__arrow_left") && counter > 0) {
         counter--;
-        carouselInner.style.transform = `translateX(${
-          -carouselInner.offsetWidth * counter
-        }px`;
+        slideWidth();
       }
-      showMeArrow();
     }
     switchSlider();
   });
 }
+
+// function initCarousel() {
+//   let carouselInner = document.querySelector(".carousel__inner");
+//   document.querySelector(".carousel__arrow_left").style.display = "none";
+
+//   let counter = 0;
+
+//   document.addEventListener("click", function (event) {
+//     let btnRight = event.target.closest(".carousel__arrow_right");
+//     let btnLeft = event.target.closest(".carousel__arrow_left");
+//     if (btnRight && counter >= 2) {
+//       btnRight.style.display = "none";
+//     }
+//     if (btnRight && counter < 3) {
+//       counter++;
+//       carouselInner.style.transform = `translateX(${
+//         -carouselInner.offsetWidth * counter
+//       }px`;
+//       document.querySelector(".carousel__arrow_left").style.display = "";
+//     } else if (btnLeft && counter > 0) {
+//       counter--;
+//       carouselInner.style.transform = `translateX(${
+//         -carouselInner.offsetWidth * counter
+//       }px`;
+//       document.querySelector(".carousel__arrow_right").style.display = "";
+//     }
+//   });
+// }
