e6a65455b99eddf89deabe51b1c1afa3e1f45e73
Konstantin <fandeluk.k@gmail.com>
2020-04-23 16:45:29 +0300

    Переделал задачу для проверки

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,27 +1,38 @@
 function initCarousel() {
   let carouselInner = document.querySelector(".carousel__inner");
   document.querySelector(".carousel__arrow_left").style.display = "none";
+  let slideElement = document.querySelectorAll(".carousel__slide").length;
 
   let counter = 0;
 
   document.addEventListener("click", function (event) {
     let btnRight = event.target.closest(".carousel__arrow_right");
     let btnLeft = event.target.closest(".carousel__arrow_left");
-    if (btnRight && counter >= 2) {
-      btnRight.style.display = "none";
+
+    function showMeArrow() {
+      counter > 0
+        ? (btnLeft.style.display = "")
+        : (btnLeft.style.display = "none");
+      counter < slideElement - 1
+        ? (btnRight.style.display = "")
+        : (btnRight.style.display = "none");
     }
-    if (btnRight && counter < 3) {
-      counter++;
-      carouselInner.style.transform = `translateX(${
-        -carouselInner.offsetWidth * counter
-      }px`;
-      document.querySelector(".carousel__arrow_left").style.display = "";
-    } else if (btnLeft && counter > 0) {
-      counter--;
-      carouselInner.style.transform = `translateX(${
-        -carouselInner.offsetWidth * counter
-      }px`;
-      document.querySelector(".carousel__arrow_right").style.display = "";
+
+    function switchSlider() {
+      if (btnRight && counter < slideElement - 1) {
+        counter++;
+        carouselInner.style.transform = `translateX(${
+          -carouselInner.offsetWidth * counter
+        }px`;
+      }
+      if (btnLeft && counter > 0) {
+        counter--;
+        carouselInner.style.transform = `translateX(${
+          -carouselInner.offsetWidth * counter
+        }px`;
+      }
+      showMeArrow();
     }
+    switchSlider();
   });
 }
