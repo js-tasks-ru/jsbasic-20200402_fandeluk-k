024ae8a71d1d3b6afd89c1142f0d7dcd91a032db
Konstantin <fandeluk.k@gmail.com>
2020-04-24 12:45:33 +0300

    Новое решение fix Новое решение

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -8,21 +8,21 @@ function initCarousel() {
   let carouselArrow = document.querySelectorAll(".carousel__arrow");
   //
   let arrowLeft = document.querySelector(".carousel__arrow_left");
-  arrowLeft.style.display = "none";
+  // arrowLeft.style.display = "none";
   let arrowRight = document.querySelector(".carousel__arrow_right");
 
   for (let arrow of carouselArrow) {
     arrow.addEventListener("click", function (event) {
       let clickRight = event.target.closest(".carousel__arrow_right");
       let clickLeft = event.target.closest(".carousel__arrow_left");
+      //
 
       if (clickRight) {
         switchRight();
       } else if (clickLeft) {
         switchLeft();
       }
-
-      console.log(counter, slideElement - 1);
+      //
       function switchRight() {
         if (counter < slideElement - 1) {
           counter++;
@@ -35,6 +35,7 @@ function initCarousel() {
       function switchLeft() {
         if (counter > 0) {
           counter--;
+          console.log(counter);
           carouselInner.style.transform = `translateX(${
             -carouselInner.offsetWidth * counter
           }px`;
