1759cb7a827a1985f001e43b9f18b64587b23059
Konstantin <fandeluk.k@gmail.com>
2020-04-28 15:34:37 +0300

    6 Модуль задача 1

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -5,11 +5,11 @@
  * Пример одного элемента, описывающего строку таблицы
  *
  *      {
-     *          name: '',
-     *          age: 25,
-     *          salary: '1000',
-     *          city: 'Petrozavodsk'
-     *   },
+ *          name: '',
+ *          age: 25,
+ *          salary: '1000',
+ *          city: 'Petrozavodsk'
+ *   },
  *
  * @constructor
  */
@@ -30,5 +30,34 @@
  */
 export default class UserTable {
   constructor(rows) {
+    let table = document.createElement("table");
+    this.elem = table;
+    this.searchItem(rows);
+    this.elem.addEventListener("click", (event) => this.onClick(event));
+  }
+  searchItem(rows) {
+    for (let item of rows) {
+      this.name = item.name;
+      this.age = item.age;
+      this.salary = item.salary;
+      this.city = item.city;
+
+      let template = ` <tr>
+          <td>${this.name}</td>
+          <td>${this.age}</td>
+          <td>${this.salary}</td>
+          <td>${this.city}</td>
+          <td><button>X</button></td>
+      </tr>
+      `;
+      this.elem.insertAdjacentHTML(`beforeend`, template);
+    }
+  }
+
+  onClick(event) {
+    let trDel = event.target.closest("tr");
+    if (event.target.closest("button")) {
+      trDel.style.display = "none";
+    }
   }
 }
